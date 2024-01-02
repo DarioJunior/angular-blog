2d1736852263ad1df98c0e81308156221a2c7eea
Dário Junior <dario.junior.rs@gmail.com>
2024-01-02 16:44:46 -0300

    feat: create  mee menu-title component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,27 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import {
+  BigCardComponent,
+  MenuBarComponent,
+  MenuTitleComponent,
+  SmallCardComponent
+ } from './components/index'
+
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    BigCardComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    SmallCardComponent,
+  ],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'angular-blog';
+  title = "angular-blog";
 }
---
+++ src/app/components/index.ts
@@ -0,0 +1,4 @@
+export { BigCardComponent } from "./big-card/big-card.component";
+export { MenuBarComponent } from "./menu-bar/menu-bar.component";
+export { MenuTitleComponent } from "./menu-title/menu-title.component";
+export { SmallCardComponent } from "./small-card/small-card.component";
